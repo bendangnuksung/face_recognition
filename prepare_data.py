7672bbbed2280be12896088dd7aadb7faf8063ba
bendangnuksung <bendangnuksungimsong@gmail.com>
2018-06-20 19:05:15 +0530

    update prepare_data

--- prepare_data.py
+++ prepare_data.py
@@ -1,147 +1,154 @@
 import cv2
+import face_recognition
+import pickle
+import argparse
 import os
-from skimage import feature, exposure
-import json
-import numpy as np
-from detect_face import get_face, find_face_1
-import dlib
-from imutils import face_utils
 
 
-IMAGE_TYPES = ['jpg', 'png', 'jpeg']
-VIDEO_TYPES = ['mp4', 'avi']
-BULK_DATA = []
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--data_path", default="data/raw_data/", help="path of the data folder")
+args = vars(ap.parse_args())
+
+prepared_data_path = "data/prepared_data/prepare_data.json"
 
 
-RAW_DATA_PATH = "data/raw_data/"
-PREPAPRED_DATA_PATH = "data/prepared_data/"
-
-ONEHOT_LABEL_PATH = "onehot_label.txt"
-
-BATCH_SIZE = 3000
-
-MAX_HEIGHT = 600
-MAX_WIDTH = 600
+image_types = ["jpg", "jpeg", "png", "tif"]
+video_types = ["mp4", "mpeg", "avi", "flv", "wmv", "mov"]
 
 
-face_landmark_model_path = 'prebuilt_model/shape_predictor_68_face_landmarks.dat'
-predictor = dlib.shape_predictor(face_landmark_model_path)
+VIDEO_FRAME_SKIP_COUNTER = 100
 
 
-def display(image):
-    cv2.imshow('img', image)
-    cv2.waitKey(0)
+def dump_serialize_encodings(data):
+    """
+    Dump Encodings to 'prepared_data_path'
+    :param data:
+    :return:
+    """
+    with open(prepared_data_path, "wb") as f:
+        f.write(pickle.dumps(data))
 
 
-# One hot encoding
-def one_hot_encoding(names):
-    length = len(names)
-    ohc = {}
-    for i,name in enumerate(names):
-        vector = []
-        for j in range(length):
-            if j==i:
-                vector.append(1)
-            else:
-                vector.append(0)
-        ohc[name] = vector
-    return ohc
+def get_media_type(file):
+    """
+    Checks if file type is:
+    1) Image
+    2) Video
+    :param file:
+    :return:
+    """
+    file = file.split("/")[-1]
+    file_type = file.split(".")[-1]
+
+    if file_type in image_types:
+        return "image"
+    elif file_type in video_types:
+        return "video"
+    else:
+        return None
 
 
-COUNTER = 1
-def dump_data(data):
-    global COUNTER
-    file_name = str(COUNTER)+".npy"
-    file_path = os.path.join(PREPAPRED_DATA_PATH, file_name)
-    np.save(file_path, data)
-    COUNTER += 1
+def detect_face(image, train=False):
+    """
+    Given an image searches for face
+    :param image:
+    :param train:
+    :return: coordinates list of the faces [( X1, Y2, X2, Y1 ) ,.... ]- format
+    """
+    if train:
+        method = "cnn"
+    else:
+        method = "hog"
+
+    bounding_boxes = face_recognition.face_locations(image, model=method)
+    return bounding_boxes
 
 
-def feature_extraction_by_video(video, label):
-    count = 1
+def encode_face(image, bounding_boxes):
+    """
+    Given an image and the coordinates gets 128 features
+    :param image:
+    :param bounding_boxes:
+    :return: 128 features of each cooridinates
+    """
+    encodings = face_recognition.face_encodings(image, bounding_boxes)
+    return encodings
+
+
+def encode_by_image(image, is_detect=False):
+    if is_detect:
+        bounding_boxes = detect_face(image)
+    else:
+        bounding_boxes = [(0, image.shape[1], image.shape[0], 0)]
+
+    if len(bounding_boxes) == 1:
+        # An image should contain only one FACE
+        image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+        encoding = encode_face(image, bounding_boxes)
+        return encoding
+    else:
+        return None
+
+
+def encode_by_video(video, skips=VIDEO_FRAME_SKIP_COUNTER):
+    encodings = []
     while True:
+        for i in range(skips):
+            _, _ = video.read()
         flag, image = video.read()
-        flag, image = video.read()
-        flag, image = video.read()
+
         if not flag:
             break
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        # display(image)
-        feature_extraction_by_image(image, label)
-        print(count)
-        count += 1
+
+        encoding = encode_by_image(image, is_detect=True)
+        if encoding != None:
+            encodings += encoding
+
+    return encodings
 
 
-def feature_extraction_by_image(image, label):
-    global BULK_DATA
-    try:
-        if image.shape[0] > MAX_HEIGHT and image.shape[1] > MAX_WIDTH:
-            image = cv2.resize(image, (int(image.shape[1]/2), int(image.shape[0]/2)))
+def prepare_data():
+    folders = os.listdir(args["data_path"])
 
-        image_1 = cv2.flip(image, 1)
-        rectangles_1 = find_face_1(image_1)
-        rectangles = find_face_1(image)
-        for rect,r in zip(rectangles, rectangles_1):
-            face_feature = predictor(image, rect)
-            face_feature = face_utils.shape_to_np(face_feature)
+    names = []
+    encodings = []
 
-            face_feature_1 = predictor(image_1, r)
-            face_feature_1 = face_utils.shape_to_np(face_feature_1)
+    for folder in folders:
+        name = folder
+        folder_path = os.path.join(args["data_path"], folder)
+        files = os.listdir(folder_path)
+        print("Reading from ", folder_path)
 
-            if len(face_feature) > 0:
-                BULK_DATA.append([face_feature, label])
-            if len(face_feature_1) > 0:
-                BULK_DATA.append([face_feature_1, label])
+        for i, file in enumerate(files, 1):
+            file_path = os.path.join(folder_path, file)
+            file_type = get_media_type(file)
 
-            if len(BULK_DATA) >= BATCH_SIZE:
-                dump_data(BULK_DATA)
-                BULK_DATA = []
+            if file_type == "image":
+                try:
+                    image = cv2.imread(file_path, 0)
+                    encode_value = encode_by_image(image)
+                    if encode_value != None:
+                        names.append(name)
+                        encodings.append(encode_value)
+                except Exception as e:
+                    print("Error at Image Encoding:", e)
 
-    except Exception as e:
-        print("Error: ",e)
+            elif file_type == "video":
+                try :
+                    video = cv2.VideoCapture(file_path)
+                    encoded_values = encode_by_video(video)
 
+                    for encoded_value in encoded_values:
+                        names.append(name)
+                        encodings.append(encoded_value)
+                except Exception as e:
+                    print("Error at Video Encoding:", e)
 
-def check_media(file):
-    media_format = file.split(".")[-1]
-    if media_format in IMAGE_TYPES:
-        return 1
-    elif media_format in VIDEO_TYPES:
-        return 2
-    else:
-        return 0
+            print("%s / %s" % (i, len(files)))
 
-
-def prepare():
-    global BULK_DATA
-    labels = os.listdir(RAW_DATA_PATH)
-    labels = one_hot_encoding(labels)
-    with open(ONEHOT_LABEL_PATH, "w") as f:
-        f.write(json.dumps(labels))
-    for i, (key, label) in enumerate(labels.items(), 1):
-        print("Folder: ",key)
-        label = np.asarray(label)
-        label_path = os.path.join(RAW_DATA_PATH, key)
-        files = os.listdir(label_path)
-        for j, file in enumerate(files):
-            file_path = os.path.join(label_path, file)
-
-            type = check_media(file)
-            if type == 1:
-                image = cv2.imread(file_path, 0)
-                feature_extraction_by_image(image, label)
-
-            elif type == 2:
-                print("Processing Video: ", file_path)
-                video = cv2.VideoCapture(file_path)
-                feature_extraction_by_video(video, label)
-
-            print(" %s  / %s"%(j, len(files)))
-
-
-        if i == len(labels):
-            dump_data(BULK_DATA)
-            BULK_DATA = []
+    name_encoding_dict = {"names": names, "encodings": encodings}
+    dump_serialize_encodings(name_encoding_dict)
 
 
 if __name__ == "__main__":
-    prepare()
+    prepare_data()
